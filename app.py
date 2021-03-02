5aa83eb03454d0565e7ba485399bb1e85cafc615
rawanebeidat <r.ebeidat@hotmail.com>
2021-03-02 01:56:52 +0000

    add some css style

--- app.py
+++ app.py
@@ -464,4 +464,4 @@ def search():
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=False)
+            debug=True)
