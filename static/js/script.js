0ef49c4393734acad12a6be18b725264aaa617de
rawanebeidat <r.ebeidat@hotmail.com>
2021-03-02 19:13:15 +0000

    add script

--- static/js/script.js
+++ static/js/script.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     $(".dropdown-trigger").dropdown();
     $('select').formSelect();
 
-    //CREDITS: The approach of validation select option for a dropdown is taken from
+    //validation select option for a dropdown is taken from
     //StackOverflow: https://stackoverflow.com/questions/34248898/how-to-validate-select-option-for-a-materialize-dropdown
     $("select[required]").css({ display: "block", height: 0, padding: 0, width: 0, position: "absolute" });
     $('textarea#recipe_description,input#recipe_name').characterCounter();
